f135d887e73c0d5ea43e6d24b8b9bafa0cc7f84a
Abdelrhman143 <aa632481@gmail.com>
2025-12-08 18:27:10 +0200

    add shadcn lib to the project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+</script>
 
 <template>
+  <link href="/src/style.css" rel="stylesheet" />
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a class="bg-red-700" href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a>
+    to read the documentation
   </p>
+  <TheHeader />
 </template>
-
-<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <h1 class="bg-amber-950">this compnent</h1>
+</template>
